4cf175beaac71e7b9f471bd099a67313870b84e7
rixx3n <orxannnuriyev@gmail.com>
2022-11-08 10:45:30 +0400

    Updates

---
+++ precache-manifest.4e61b10ec90e813d8af80cf5c4ebdbda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fabee83e9ce8065e56e5",
+    "url": "/markdown/static/css/main.a1cb0e42.chunk.css"
+  },
+  {
+    "revision": "fabee83e9ce8065e56e5",
+    "url": "/markdown/static/js/main.fabee83e.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/markdown/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "aa4e8cc1cff98e421094",
+    "url": "/markdown/static/js/2.aa4e8cc1.chunk.js"
+  },
+  {
+    "revision": "acc5760ab425e44a62d5",
+    "url": "/markdown/static/js/runtime~main.acc5760a.js"
+  },
+  {
+    "revision": "23e64ef8e8a4007481eff9f3f4c771a5",
+    "url": "/markdown/index.html"
+  }
+];
